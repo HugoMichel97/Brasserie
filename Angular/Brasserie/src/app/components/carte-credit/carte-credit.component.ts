59ad9797a3f20c8349f99ff053bcbd67c11608c8
amineber97 <amine.berrada19971120@gmail.com>
2022-04-13 17:00:20 +0200

    visu carte credit

--- Angular/Brasserie/src/app/components/carte-credit/carte-credit.component.ts
+++ Angular/Brasserie/src/app/components/carte-credit/carte-credit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-carte-credit',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carte-credit.component.css']
 })
 export class CarteCreditComponent implements OnInit {
+
+  @Input() cardNumber?: string;
+  @Input() clientName?: string;
+  @Input() expirationDate?: string;
 
   constructor() { }
 
